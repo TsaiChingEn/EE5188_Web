5c3ce97e2c0080087d47e813fdb82d15ae7369eb
TsaiChingEn <b085040039@gmail.com>
2024-03-23 02:56:36 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -62,13 +62,12 @@ const App = () => {
       )}
       {currentPage === 'introduction' && (
         <div className="introduction">
-          <h3>
+          <div>
             <img src="photo3.png" alt="Introduction" className="intro-photo" align="center"/>
-            Welcome!
-            <h5>Welcome to our message board. Feel free to leave a message!</h5>
-          </h3>
+            <p>Welcome!</p>
+            <p>Welcome to our message board. Feel free to leave a message!</p>
+          </div>
           <p>Welcome to our message board. Feel free to leave a message!</p>
-          <p>This is a platform where you can share your thoughts and connect with others.</p>
         </div>
       )}
     </div>
